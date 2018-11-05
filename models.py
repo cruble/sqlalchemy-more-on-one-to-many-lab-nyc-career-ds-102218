d437ebc98a17eb89dd456c58dbeff184899e49d6
Chad Ruble <cruble4@gmail.com>
2018-11-05 18:10:05 -0500

    database up and associated

--- models.py
+++ models.py
@@ -6,10 +6,54 @@ Base = declarative_base()
 
 
 # write the Player, City, Sport and Team tables below
+class Player(Base):
+	__tablename__ = "players"
+
+	id = Column(Integer, primary_key=True)
+	name = Column(Text)
+	age = Column(Integer)
+	number = Column(Integer)
+	height = Column(Text)
+	weight = Column(Integer)
+	birthday = Column(String)
+	team_id = Column(Integer, ForeignKey('teams.id'))
+	team = relationship('Team', back_populates='players')
+
+class Team(Base):
+	__tablename__ = "teams"
+
+	id = Column(Integer, primary_key=True)
+	name = Column(Text)
+
+
+	city_id = Column(Integer, ForeignKey('cities.id'))
+	city = relationship('City', back_populates='teams')
+
+	sport_id = Column(Integer, ForeignKey('sports.id'))
+	sport = relationship('Sport', back_populates='teams')
+
+	players = relationship(Player, back_populates='team')
 
 
 
 
+class Sport(Base):
+	__tablename__ = 'sports'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(Text)
+
+	teams = relationship(Team, back_populates='sport')
+
+
+class City(Base):
+	__tablename__ = 'cities'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(Text)
+	state = Column(Text)
+
+	teams = relationship(Team, back_populates='city')
 
 
 
--- seed.py
+++ seed.py
@@ -1,8 +1,10 @@
 from models import *
 from sqlalchemy import create_engine
 import pandas as pd
+import pdb
+import datetime 
 
-engine = create_engine('sqlite:///actors.db')
+engine = create_engine('sqlite:///sports.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -18,7 +20,7 @@ session = Session()
 # la_dodgers0 = pd.read_csv('la_dodgers_baseball.csv')
 # la_dodgers1 = pd.read_csv('la_dodgers_baseball.csv').to_dict()
 # la_dodgers2 = pd.read_csv('la_dodgers_baseball.csv').to_dict(orient='records')
-# import pdb; pdb.set_trace()
+
 # __________________
 la_dodgers = pd.read_csv('la_dodgers_baseball.csv').to_dict(orient='records')
 la_lakers = pd.read_csv('la_lakers_basketball.csv').to_dict(orient='records')
@@ -30,3 +32,81 @@ ny_knicks = pd.read_csv('ny_knicks_basketball.csv').to_dict(orient='records')
 # add and commit the players, teams, sports and cities below
 # we will need to probably write at least one function to iterate over our data and create the players
 # hint: it may be a good idea to creat the Teams, Cities, and Sports first 
+
+ny = City(name="New York", state="New York")
+la = City(name="Los Angeles", state="California")
+
+dodgers = Team(name ="Dodgers")
+yankees = Team(name ="Yankees")
+knicks = Team(name = "Knicks")
+lakers = Team(name = "Lakers")
+
+basketball = Sport(name = "basketball")
+baseball = Sport(name = "baseball")
+
+dodgers.city = la 
+yankees.city = ny 
+knicks.city = ny 
+lakers.city = la 
+
+dodgers.sport = baseball 
+yankees.sport = baseball
+knicks.sport = basketball
+lakers.sport = basketball
+
+# pdb.set_trace()
+session.add_all([ny, la, dodgers, yankees, knicks, lakers, basketball, baseball])
+session.commit() 
+
+now = datetime.date.today()
+commit_list = []
+
+for dodger in la_dodgers: 
+	bday = dodger['birthdate']
+	bday_list = bday.split('/')
+	year = bday_list[2]
+	if len(year) < 4:
+		year = "19" + year 
+	bday_date = datetime.date(int(year), int(bday_list[0]), int(bday_list[1]))
+	age = now.year - bday_date.year  
+
+	new_dodger = Player(name = dodger['name'], number = dodger['number'] , height = dodger['height'], weight = dodger['weight'], age = age)
+	new_dodger.team = dodgers
+	commit_list.append(new_dodger)
+	session.add(new_dodger)
+	session.commit()
+
+for yankee in ny_yankees: 
+	new_yankee = Player(name = yankee['name'], height = yankee['height'], weight = yankee['weight'], age = yankee['age'])
+	new_yankee.team = yankees 
+	commit_list.append(new_yankee)
+	session.add(new_yankee)
+	session.commit()
+
+for knick in ny_knicks: 
+	new_knick = Player(name = knick['name'], height = knick['height'], weight = knick['weight'], age = knick['age'], number = knick['number'])
+	new_knick.team = knicks 
+	commit_list.append(new_knick)
+	session.add(new_knick)
+	session.commit()
+
+for laker in la_lakers:
+	new_laker = Player(name = laker['name'], height = laker['height'], weight = laker['weight'], age = laker['age'], number = laker['number'])
+	new_laker.team = lakers 
+	commit_list.append(new_laker)
+	session.add(new_laker)
+	session.commit()
+
+# session.add_all([commit_list])
+# session.commit()
+
+
+
+
+
+
+
+
+
+
+
