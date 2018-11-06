a4225b81ebac6b4a8cfb3a2e5eadfd646a98eb7d
Chad Ruble <cruble4@gmail.com>
2018-11-05 21:18:12 -0500

    started some queries

--- queries.py
+++ queries.py
@@ -8,26 +8,27 @@ session = Session()
 
 def return_teams_for_new_york():
     # here we want to return all teams that are associated with New York City
-    pass
+    return session.query(Team).filter(team.city_id == ny.id).all()
 
 def return_players_for_la_dodgers():
     # here we want to return all players that are associated with the LA dodgers
-    pass
+    return session.query(Player).filter(Player.team_id == dodgers.id).all()
 
 def return_sorted_new_york_knicks():
     # here we want to return all the players on the New York Knicks
     # sorted in ascending (small -> big) order by their number
-    pass
+    return session.query(Player).filter(Player.team_id == knicks.id).order_by(Player.number).all()
 
 def return_youngest_basket_ball_player_in_new_york():
     # here we want to sort all the players on New York Knicks by age
     # and return the youngest player
-    pass
+    return session.query(Player).filter(Player.team_id == knicks.id).order_by(Player.age).first()
 
 def return_all_players_in_los_angeles():
     # here we want to return all players that are associated with
     # a sports team in LA
-    pass
+    # return session.query(Player, Team, City).filter(City == la).all()
+    pass 
 
 def return_tallest_player_in_los_angeles():
     # here we want to return the tallest player associted with
@@ -37,4 +38,5 @@ def return_tallest_player_in_los_angeles():
 def return_team_with_heaviest_players():
     # here we want to return the city with the players that
     # have the heaviest average weight (total weight / total players)
+    player = 
     pass
